350c7a8834928cc84d73f1f694b662e43a2929ea
Claudio-Pri <priv.claudio@gmail.com>
2024-08-28 15:45:35 +0200

    print h1, image da data

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,24 @@ console.log('js collegato');
 //===== Appunti ======
 // ` backtick
 
+// {{TEXT INTERPOLATION}}
+
 //===== Code ======
 
 
 //===== Functions =====
+
+// deconstructuring
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Ciao Vue!',
+            image: 'img/happy-dog.jpg'
+        }
+    }
+
+
+}).mount('#app')
+// 'monto' vue
